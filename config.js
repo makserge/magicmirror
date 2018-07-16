aec5b4f9b183733a831f0256f8f2c4977ec8d33e
Sergey <makserge@mail.ru>
2018-07-17 02:45:09 +0300

    Added temperatures from MQTT

--- config.js
+++ config.js
@@ -10,7 +10,8 @@
  */
 var config = {
   port: 8080,
-  ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+  ipWhitelist: ["127.0.0.1", "192.168.31.186","::1"],
+  //ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
   // or add a specific IPv4 of 192.168.1.5 :
   // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
   // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -19,71 +20,74 @@ var config = {
   timeFormat: 24,
   units: "metric",
   modules: [
-  //  {
-  //    module: "alert",
-  //  },
-  //  {
-  //    module: "updatenotification",
-  //    position: "top_bar"
-  //  },
     {
       module: "clock",
       position: "top_left",
 	  config: {
-		  displayType: "both"
+			displaySeconds: false,
+			dateFormat: "LL",
+			displayType: "digital",
+			analogPlacement: "right",
+			analogSize: "120px",
+			timezone: "Europe/Kiev"
 	  }
     },
-    /*{
-      module: "calendar",
-      header: "US Holidays",
-      position: "top_left",
-      config: {
-        calendars: [
-          {
-            symbol: "calendar-check-o ",
-            url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-          }
-        ]
-      }
-    },*/
-//    {
-//      module: "compliments",
-//      position: "lower_third"
-//    },
-    {
+ /*
+   {
       module: "currentweather",
       position: "top_right",
+	  config: {
+        location: "Киев",
+        locationID: "703448", //Kiev; //ID from http://www.openweathermap.org/help/city_list.txt
+        appid: ""
+      }
+    },
+    */
+    {
+      module: 'MMM-MQTT', //https://github.com/ottopaulsen/MMM-MQTT
+      position: 'top_right',
+      header: 'Температура',
       config: {
-        location: "Kiev",
-        locationID: "703448", //ID from http://www.openweathermap.org/help/city_list.txt
-        appid: "bd5e378503939ddaee76f12ad7a97608"
+        mqttUser: '',         // Leave out for no user
+        mqttPassword: '', // Leave out for no password
+        mqttServer: 'localhost',
+        subscriptions: [
+          {
+            topic: 'zigbee2mqtt/external_temp',
+            label: 'Улица',
+            decimals: 1,
+            suffix: '°C',
+            jsonpointer: '/temperature'
+          },
+          {
+            topic: 'zigbee2mqtt/living_room_temp',
+            label: 'Комната',
+            decimals: 1,
+            suffix: '°C',
+            jsonpointer: '/temperature'
+          },
+          {
+            topic: 'zigbee2mqtt/kitchen_temp',
+            label: 'Кухня',
+            decimals: 1,
+            suffix: '°C',
+            jsonpointer: '/temperature'
+          }
+        ]
       }
     },
     {
       module: "weatherforecast",
-      position: "top_right",
-      header: "Weather Forecast",
+      position: "bottom_bar",
+      header: "Прогноз погоды",
       config: {
-        location: "Kiev",
-        locationID: "703448", //ID from http://www.openweathermap.org/help/city_list.txt
-        appid: "bd5e378503939ddaee76f12ad7a97608"
+		forecastEndpoint: "forecast",  
+        location: "Kiev,Ukraine",
+		maxNumberOfDays: 3,
+        locationID: "703448", //Kiev; //ID from http://www.openweathermap.org/help/city_list.txt
+        appid: ""
       }
     },
-	/*
-    {
-      module: "newsfeed",
-      position: "bottom_bar",
-      config: {
-        feeds: [
-          {
-            title: "New York Times",
-            url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-          }
-        ],
-        showSourceTitle: true,
-        showPublishDate: true
-      }
-    },*/
   ]
 };
 /*************** DO NOT EDIT THE LINE BELOW ***************/
